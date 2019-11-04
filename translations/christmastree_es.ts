14585622b8ef741f0bad88f397b424de8b365da7
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-11-05 01:12:00 +0300

    Regen translations with locations.

--- translations/christmastree_es.ts
+++ translations/christmastree_es.ts
@@ -4,14 +4,17 @@
 <context>
     <name>AdMobConsentDialog</name>
     <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="42"/>
         <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt;</source>
         <translation>Mantenemos esta aplicación gratuita mostrando anuncios. La red publicitaria &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;recopilar datos y utilizará un identificador único&lt;/a&gt; en su dispositivo para mostrarle anuncios. &lt;b&gt;¿Permites usar tus datos para personalizar anuncios para ti?&lt;/b&gt;</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="70"/>
         <source>Yes, show me relevant ads</source>
         <translation>Sí, muéstrame anuncios relevantes</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/AdMobConsentDialog.qml" line="102"/>
         <source>No, show me ads that are less relevant</source>
         <translation>No, muéstrame anuncios que sean menos relevantes</translation>
     </message>
@@ -19,22 +22,27 @@
 <context>
     <name>HelpDialog</name>
     <message>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="71"/>
         <source>How to decorate Christmas Tree</source>
         <translation>Cómo decorar el árbol de Navidad</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="72"/>
         <source>1. Press Settings button to choose how your tree and background image should look like. Press Settings button again to hide list of available options.</source>
         <translation>1. Presione el botón Configuración para elegir cómo debe verse su árbol y su imagen de fondo. Presione el botón Configuración nuevamente para ocultar la lista de opciones disponibles.</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="73"/>
         <source>2. Press Toys button to open toys gallery. Press Toys button again to hide this gallery.</source>
         <translation>2. Presione el botón de Juguetes para abrir la galería de juguetes. Presione el botón de Toys nuevamente para ocultar esta galería.</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="74"/>
         <source>3. Select toy you like from gallery by holding your finger on it and then drag selected toy to appropriate place on Christmas Tree. Press and hold your finger on the toy to send it to background or bring to foreground. Move toy away from Christmas Tree to remove it.</source>
         <translation>3. Selecciona el juguete que te gusta de la galería manteniendo tu dedo sobre él y luego arrastra el juguete seleccionado al lugar apropiado en el Árbol de Navidad. Mantenga presionado el dedo sobre el juguete para enviarlo al fondo o ponerlo en primer plano. Mueva el juguete fuera del árbol de Navidad para quitarlo.</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/HelpDialog.qml" line="75"/>
         <source>4. Repeat previous step, choosing different toys from gallery until you get your own wonderfully decorated Christmas Tree. Press Capture or Record buttons to save image or GIF of your Christmas Tree or share it with friends.</source>
         <translation>4. Repita el paso anterior, eligiendo diferentes juguetes de la galería hasta que obtenga su propio árbol de Navidad maravillosamente decorado. Presione los botones Capturar o Grabar para guardar la imagen o el GIF de su Árbol de Navidad o compartirlo con amigos.</translation>
     </message>
@@ -42,14 +50,17 @@
 <context>
     <name>PurchaseDialog</name>
     <message>
+        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="108"/>
         <source>View ad and capture image</source>
         <translation>Ver anuncio y captura de imagen</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="159"/>
         <source>Purchase full version</source>
         <translation>Comprar la versión completa</translation>
     </message>
     <message>
+        <location filename="../qml/Core/Dialog/PurchaseDialog.qml" line="210"/>
         <source>Restore purchases</source>
         <translation>Restaurar las compras</translation>
     </message>
@@ -57,6 +68,7 @@
 <context>
     <name>main</name>
     <message>
+        <location filename="../qml/main.qml" line="11"/>
         <source>Christmas</source>
         <translation>Árbol de Navidad</translation>
     </message>
